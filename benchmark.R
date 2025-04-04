9cab20422f143e0cff900e517aa88f37f20409ba
Sebastian Fischer <sebf.fischer@gmail.com>
2025-04-04 13:10:51 +0200

    ...

--- benchmark.R
+++ benchmark.R
@@ -206,7 +206,7 @@ get_result = function(ids, what) {
 summarize = function(ids = NULL) {
   jt = getJobTable() |> unwrap()
   if (!is.null(ids)) jt = jt[ids, ]
-  jt = jt[, c("n_layers", "jit", "optimizer", "batch_size", "device", "opt_type", "algorithm", "repl", "tag")]
+  jt = jt[, c("n_layers", "jit", "optimizer", "batch_size", "device", "opt_type", "algorithm", "repl", "tag", "latent")]
   jt$time = get_result(ids, "time")
   jt$loss = get_result(ids, "loss")
   jt$memory = get_result(ids, "memory") / 2^30
